05ef4fb65387a0e4688deee8a5d29092e17f7652
Enzo <dipaolae98@gmail.com>
2022-06-01 00:19:01 +0100

    theme dropdown

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,35 @@
+const Themes = ['monokai','ambiance','chaos','chrome','clouds','clouds_midnight','cobalt','dawn','dreamweaver','eclipse','github','gob','gruvbox','idle_fingers','iplastic','kr_theme','merbivore','mono_industrial','solarized_dark','solarized_light','terminal','tomorrow_night','tomorrow_night_blue','tomorrow_night_bright','twilight','vibrant_ink'];
+
+
+//gets current tab
+async function getCurrentTab() {
+    let queryOptions = { active: true, currentWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
 
 (async () => {
-    
+    //THEME DROPDOWN
+    let ul = document.getElementById("themeDropdownList");
+    for(let theme of Themes){
+        //create list element for each theme
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        a.textContent = theme;
+        a.className = "dropdown-item";
+        a.addEventListener("click", () => {
+            //if current tab has editor set theme
+            getCurrentTab().then(tab => {
+                if(tab.url.includes("/Scenes/Play/")){
+                    document.getElementById("themeDropdown").innerText = theme;
+                    //switch to theme on background script
+                    chrome.runtime.sendMessage({type:"intellisense",message:"setTheme", value:"ace/theme/"+theme});
+                }
+            })
+            
+            
+        });
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
 })()
--- src/classes-shared/Settings.js
+++ src/classes-shared/Settings.js
@@ -62,8 +62,6 @@ export class GlobalSetting {
 export class LocalSetting {
     static WORD_WRAP = new Setting('WORD_WRAP', false, true);
 
-    static Themes = ['ambiance','chaos','chrome','clouds','clouds_midnight','cobalt','crimson_editor','dawn','dracula','dreamweaver','eclipse','github','gob','gruvbox','idle_fingers','iplastic','katzenmilch','kr_theme','kuroir','merbivore','merbivore_soft','mono_industrial','monokai','nord_dark','one_dark','pastel_on_dark','solarized_dark','solarized_light','sqlserver','terminal','textmate','tomorrow','tomorrow_night','tomorrow_night_blue','tomorrow_night_bright','tomorrow_night_eighties','twilight','vibrant_ink','xcode'];
-
 
     static Get(keys){
         return new Promise((resolve, reject) => {
